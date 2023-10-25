38618f94279b12feb143b57ff1b6655a87200b97
Christian Lete <christian.lete+github@gmail.com>
2023-10-25 12:40:41 +0200

    migration of pde methods to js

--- public/sketch.js
+++ public/sketch.js
@@ -4,7 +4,8 @@ let songs = [];
 let currentSongIndex = 0;
 // Variable to store the current song title
 let songTitle = "";
-
+let length = 0;
+let angle = 0;
 function preload() {
   // List all the MP3 files in the "audio/" folder
   let songFiles = [
@@ -24,11 +25,12 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(windowWidth, 640);
-  // Set angle mode to DEGREES
-  angleMode(DEGREES);
-  // Set color mode to HSB
-  colorMode(HSB);
+  
+  //createCanvas(windowWidth, 1024);
+  // Set up the canvas size and frame rate
+ createCanvas(1024, 1024, WEBGL); // Canvas size is 1200x1200 pixels in 3D mode
+ camera(width/2, height/2-200, (height/2.0) / tan(PI*30.0 / 180.0), width/2, height/2, 0, 0, -1, 0);
+ colorMode(HSB);
   
 
 // Create a button to play the previous song
@@ -93,10 +95,28 @@ function playPreviousSong() {
 function draw() {
   // Set the background color to black
   background(0);
+  //lights();
+  frameRate(30); // Set the frame rate to 30 frames per second
+   // Camera settings
+  orbitControl(); // Enable 3D camera control
+  let cal = new Date();
+  let remainder = ((cal.getMilliseconds()) + (cal.getSeconds() * 1000)) % 20001;
+  
+  //push();
+  //fill('red');
+  //rect(0,0,1532,24);
+  //pop();
+ 
+  
+  
+  translate(width/2, 0, 0);
+  rotateY(map(remainder, 0, 20000, 0, 2*PI));
+  
+
 
   // Analyze the audio spectrum using FFT
   let spectrum = fft.analyze();
-  
+  //strokeWeight(2);  
   
   // Define a variable for the gap width between bars
 let gapWidth = 4;
@@ -114,14 +134,16 @@ for (let side = -1; side <= 1; side += 2) {
     // Map the amplitude to a vertical position on the canvas
     let y = map(amp, 0, 256, height, 0);
 
-    // Calculate the x-position for the current side (left or right)
-    let x = width / 2 + side * (i * (barWidth + gapWidth) + barWidth / 2);
+    // Calculate the x-position for the current side (left or right); 511 is a hardcoded value at this point
+    // remember to change 25.10.2023 - Christian
+    let x = (width / 2 + side * (i * (barWidth + gapWidth) + barWidth / 2)) - 511;
+    
     
     // Draw rectangles for the audio visualization on the current side
     rect(x, y, barWidth, height - y);
   }
-}
 
+}
 
   
  // Display the current song title above the chart
